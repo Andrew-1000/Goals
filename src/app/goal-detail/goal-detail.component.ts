5374b65206f8c31a920c37bd9ed389f75bc40fbe
Andrew-1000 <andruambia95@gmail.com>
2020-01-09 09:10:48 +0300

    Make changes to the styles,css and other several components

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -18,6 +18,10 @@ export class GoalDetailComponent implements OnInit {
     this.isComplete.emit(complete);
   }
 
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+
   constructor() { }
 
   ngOnInit() {
